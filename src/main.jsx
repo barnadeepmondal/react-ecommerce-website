d3d2571b92e0b90fa9300e9a71a060083c7f52fa
barnadeepmondal <barnadeepm@gmail.com>
2026-06-18 11:52:22 +0530

    Fix filename case for ProductContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import ProductContext from "./ProductContext/productContext.jsx";
-import { ProductProvider } from "./ProductContext/productContext.jsx";
+import ProductContext from "./ProductContext/productcontext.jsx";
+import { ProductProvider } from "./ProductContext/productcontext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <ProductProvider>
