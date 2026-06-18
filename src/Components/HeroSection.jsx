7c9964629f83feb72ef888edfcd1b4096c376535
barnadeepmondal <barnadeepm@gmail.com>
2026-06-18 13:25:48 +0530

    fix hero image clean rename

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,9 +1,9 @@
-import heroImg from "../assets/heroImg.jpg"
+import temp from "../assets/temp.jpg"
 const HeroSection = () => {
   return (
     <>
         <section className="heroContainer">
-            <img src={heroImg} alt="A brown colour background on that background it is written ecommerce"  className="w-full object-cover" />
+            <img src={temp} alt="A brown colour background on that background it is written ecommerce"  className="w-full object-cover" />
         </section>
     
     </>
