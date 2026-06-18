ae8ab703398998a90412e7ec5705a4487ffe8847
barnadeepmondal <barnadeepm@gmail.com>
2026-06-18 12:12:34 +0530

    Fix hero image path again

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,9 +1,9 @@
-import heroImg from "../assets/heroImg.jpg"
+import heroImg from "../assets/heroimg.jpg"
 const HeroSection = () => {
   return (
     <>
         <section className="heroContainer">
-            <img src={heroImg} alt="A brown colour background on that background it is written ecommerce"  className="w-full object-cover" />
+            <img src={heroimg} alt="A brown colour background on that background it is written ecommerce"  className="w-full object-cover" />
         </section>
     
     </>
