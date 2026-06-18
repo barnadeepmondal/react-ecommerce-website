a0a66313e59a3ebbba5fb853085ba680c3f3b428
barnadeepmondal <barnadeepm@gmail.com>
2026-06-18 11:49:27 +0530

    Fix import path case

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import ProductContext from "./productContext/productContext.jsx";
-import { ProductProvider } from "./productContext/productContext.jsx";
+import ProductContext from "./ProductContext/productContext.jsx";
+import { ProductProvider } from "./ProductContext/productContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <ProductProvider>
